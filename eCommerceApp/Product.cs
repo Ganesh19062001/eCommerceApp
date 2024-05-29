ccd67e556831fc2bd849aa7d3116ffe19d7c3dbd
Ganesh Babu G <Ganesh Babu G@GANESH-PC>
2024-05-29 11:26:07 -0500

    Added product class with baic properties and methods.

--- eCommerceApp/Product.cs
+++ eCommerceApp/Product.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace eCommerceApp
 {
-    internal class Product
+    public class Product
     {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            Stock += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount <= Stock)
+            {
+                Stock -= amount;
+            }
+            else
+            {
+                throw new ArgumentException("Amount to decrease exceeds stock available");
+            }
+        }
     }
 }
+
